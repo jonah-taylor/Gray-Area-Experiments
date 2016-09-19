7ea1abadc53c0b27de22d126340ce0894eb426af
jonah-taylor <jonahtaylor24@gmail.com>
2016-09-18 17:36:05 -0700

    added jQuery that changes the h1 on click

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Changes Header Title opacity to .5
 $(function(){
 	$("#header-title").css("opacity","0.5");
 }); 
+
+//Makes By Rusty button change the Header Title
+ $(function(){
+            $("a#button").click(function(){
+                $("h1#header-title").text("By Rusty!!!!!!!");
+            });            
+        });
