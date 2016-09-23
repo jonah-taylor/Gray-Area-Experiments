23a4b7ec0ef7fb4c12eeaf463bf7ff7168a0c683
jonah-taylor <jonahtaylor24@gmail.com>
2016-09-22 21:47:00 -0700

    Things Rusty Likes Styled By jQuery

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 //Changes Header Title opacity to .5
-$(function(){
-	$("#header-title").css("opacity","0.5");
-}); 
+//$(function(){
+//	$("#header-title").css("opacity","0.5");
+//}); 
 
 //Makes By Rusty button change the Header Title
- $(function(){
-            $("a#button").click(function(){
-                $("h1#header-title").text("By Rusty!!!!!!!");
-            });            
-        });
+// $(function(){
+//            $("a#button").click(function(){
+//                $("h1#header-title").text("By Rusty!!!!!!!");
+//            });            
+//        });
+
+//let start at index 0 
+//if index < length
+//do the stuff
+//then index++
+
+var listItems = $("li");
+
+$(function(){
+	
+	var listItems = $("li");
+	var red = 150;
+	for (var index = 1; index <= listItems.length; index++ ) {
+		var indexAsString = index.toString();		
+		var opacity = (index/10).toString();
+		var rgb = "rgb(" + red.toString() + ",100,255)"
+		$("#list-" + indexAsString).css( "color", rgb );
+		$("#list-" + indexAsString).css( "opacity", opacity );
+		red += 25;
+	}
+});
+  
+//  $(function(){	
+//	for (var index = 0; index < 0; index++ ) {
+//		
+//	}
+//});
