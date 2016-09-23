b6c92eba6ddc92e9317cbd39801ce95d75132c84
jonah-taylor <jonahtaylor24@gmail.com>
2016-09-23 09:41:12 -0700

    Beautified script.js and uncommented functions from earlier jQuery exercise

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-//Changes Header Title opacity to .5
-//$(function(){
-//	$("#header-title").css("opacity","0.5");
-//}); 
+//Changes header title opacity to .5
+$(function () {
+	$("#header-title").css("opacity", "0.5");
+});
 
-//Makes By Rusty button change the Header Title
-// $(function(){
-//            $("a#button").click(function(){
-//                $("h1#header-title").text("By Rusty!!!!!!!");
-//            });            
-//        });
+//Makes by rusty button change the header title
+$(function () {
+	$("a#button").click(function () {
+		$("h1#header-title").text("By Rusty!!!!!!!");
+	});
+});
 
-//let start at index 0 
-//if index < length
-//do the stuff
-//then index++
-
+//Iterates over list items with change in opacity and color gradient 
 var listItems = $("li");
 
-$(function(){
-	
+$(function () {
+
 	var listItems = $("li");
 	var red = 150;
-	for (var index = 1; index <= listItems.length; index++ ) {
-		var indexAsString = index.toString();		
-		var opacity = (index/10).toString();
+	for (var index = 1; index <= listItems.length; index++) {
+		var indexAsString = index.toString();
+		var opacity = (index / 10).toString();
 		var rgb = "rgb(" + red.toString() + ",100,255)"
-		$("#list-" + indexAsString).css( "color", rgb );
-		$("#list-" + indexAsString).css( "opacity", opacity );
+		$("#list-" + indexAsString).css("color", rgb);
+		$("#list-" + indexAsString).css("opacity", opacity);
 		red += 25;
 	}
 });
-  
-//  $(function(){	
-//	for (var index = 0; index < 0; index++ ) {
-//		
-//	}
-//});
